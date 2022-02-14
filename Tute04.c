fc7bfcc652f252fef887a478e9d893acaa5262ae
it21171260 <99630973+it21171260@users.noreply.github.com>
2022-02-14 16:46:09 +0530

    Update Tute04.c

--- Tute04.c
+++ Tute04.c
@@ -6,14 +6,39 @@ Do not change the code given in the main() function when you are implementing yo
 
 #include <stdio.h>
 
+#include <stdio.h>
+
+int max(int no1, int no2);
+int min(int no1, int no2);
+int multi (int no1,int no2);
+
 int main() {
-   int no1, no2;
-   printf("Enter a value for no 1 : ");
-   scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
-   scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
-   return 0;
+	int no1, no2, maximum, minimum, multiply;
+
+	printf("Enter a value for no 1 : ");
+	scanf("%d", &no1);
+	printf("Enter a value for no 2 : ");
+	scanf("%d", &no2);
+
+	maximum = max(no1, no2);
+	minimum = min(no1, no2);
+	multiply = multi(no1,no2);
+
+	printf("\nMaximum = %d", maximum);
+	printf("\nMinimum = %d", minimum);
+	printf("\nMultiply = %d", multiply);
+
+	return 0;
+}
+
+int max(int no1, int no2) {
+	return (no1 > no2) ? no1 : no2;
+}
+
+int min(int no1, int no2) {
+	return (no1 > no2) ? no2 : no1;
+}
+
+int multi(int no1, int no2) {
+	return (no1* no2);
 }
