d452d638b9ad54b07f3c5af5377da33045a5440f
it21171260 <99630973+it21171260@users.noreply.github.com>
2022-02-14 16:43:22 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,15 @@
 #include <stdio.h>
 
 int main() {
-  
+   int mark1,mark2;
+
+   printf("input mark 1 : ");
+   scanf("%d",&mark1);
+   printf("input mark 2 : ");
+   scanf("%d",&mark2);
+
+   printf("your mark avarage is : %.2f",(float)(mark1+mark2)/2);
+
   return 0;
 }
 
